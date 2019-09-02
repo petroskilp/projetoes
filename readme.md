9f782883797a5d50544f17685e8a44248d1db31e
Luiz Pedro Petroski <petroskilp@gmail.com>
2019-09-02 18:26:16 -0300

    Atualizando

--- readme.md
+++ readme.md
@@ -1,72 +1,149 @@
-<p align="center"><img src="https://laravel.com/assets/img/components/logo-laravel.svg"></p>
+# Projeto Engenharia de Software NODE
 
-<p align="center">
-<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/d/total.svg" alt="Total Downloads"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/v/stable.svg" alt="Latest Stable Version"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
-</p>
+Exemplo de projeto utilizando Node.js.
 
-## About Laravel
+Este exemplo é um aplicativo web que pode ser executado localmente assim como realizar o deploy em hosts de homologação/produção.
+*   Para o guia completo deste projeto acesse o arquivo README.md do repositório do [Projeto](https://github.com/engenhariadesoftwareuepg/projetophp).
+*   Este projeto utiliza o framework web [Laravel](https://laravel.com/)
+*   Está configurado com o banco de dados relacional PostgresSQL, utilizado o [Eloquent ORM](https://laravel.com/docs/5.8/eloquent) incluído no Laravel para modelagem dos dados e mapeamento
+*   Para a definição das views e templates é utilizado a engine [Blade](https://laravel.com/docs/5.8/blade)
+*   Também está implmentado alguns testes de feature com o PHPUnit. [Testando no laravel](https://laravel.com/docs/5.8/testing)
+  
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
+  
 
-- [Simple, fast routing engine](https://laravel.com/docs/routing).
-- [Powerful dependency injection container](https://laravel.com/docs/container).
-- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
-- Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).
-- Database agnostic [schema migrations](https://laravel.com/docs/migrations).
-- [Robust background job processing](https://laravel.com/docs/queues).
-- [Real-time event broadcasting](https://laravel.com/docs/broadcasting).
+## Arquitetura da aplicação
 
-Laravel is accessible, powerful, and provides tools required for large, robust applications.
+  
 
-## Learning Laravel
+*   Esta aplicação utiliza uma arquitetura MVC (Model, View e Control)
 
-Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.
+*   O **Model** é responsável pela estrutura de dados da aplicação, e neste caso utiliza o Eloquent Model para modelagem dos dados. Os arquivos de Models fica no diretório `/app/`
+*   O **Controller** é responsável pela regra de negócio, assim como instanciar os models e gerenciar os dados para renderizar as views. O controller fica no diretório `/app/Http/controllers/`
+*   As **Views** tem a responsabilidade de renderizar e mostrar as informações oriundas do model e é controlada pelo controller. O diretório de localização das views é `/resources/views/`
 
-If you don't feel like reading, [Laracasts](https://laracasts.com) can help. Laracasts contains over 1400 video tutorials on a range of topics including Laravel, modern PHP, unit testing, and JavaScript. Boost your skills by digging into our comprehensive video library.
+*   Todas as rotas, ou seja as urls que serão utilizadas para acessar cada função no sistema são definidas no arquivo `/routes/web.php`
+*   Os testes são executados todos arquivos que tem o sufixo Test.php que estão localizados nos diretórios `/Feature/` e `/Unit/`
+  
 
-## Laravel Sponsors
+## Banco de dados
 
-We would like to extend our thanks to the following sponsors for funding Laravel development. If you are interested in becoming a sponsor, please visit the Laravel [Patreon page](https://patreon.com/taylorotwell).
+  
 
-- **[Vehikl](https://vehikl.com/)**
-- **[Tighten Co.](https://tighten.co)**
-- **[Kirschbaum Development Group](https://kirschbaumdevelopment.com)**
-- **[64 Robots](https://64robots.com)**
-- **[Cubet Techno Labs](https://cubettech.com)**
-- **[Cyber-Duck](https://cyber-duck.co.uk)**
-- **[British Software Development](https://www.britishsoftware.co)**
-- **[Webdock, Fast VPS Hosting](https://www.webdock.io/en)**
-- **[DevSquad](https://devsquad.com)**
-- [UserInsights](https://userinsights.com)
-- [Fragrantica](https://www.fragrantica.com)
-- [SOFTonSOFA](https://softonsofa.com/)
-- [User10](https://user10.com)
-- [Soumettre.fr](https://soumettre.fr/)
-- [CodeBrisk](https://codebrisk.com)
-- [1Forge](https://1forge.com)
-- [TECPRESSO](https://tecpresso.co.jp/)
-- [Runtime Converter](http://runtimeconverter.com/)
-- [WebL'Agence](https://weblagence.com/)
-- [Invoice Ninja](https://www.invoiceninja.com)
-- [iMi digital](https://www.imi-digital.de/)
-- [Earthlink](https://www.earthlink.ro/)
-- [Steadfast Collective](https://steadfastcollective.com/)
-- [We Are The Robots Inc.](https://watr.mx/)
-- [Understand.io](https://www.understand.io/)
-- [Abdel Elrafa](https://abdelelrafa.com)
-- [Hyper Host](https://hyper.host)
+É utilizado um banco de dados PostgreSQL. Você poderá utilizar para rodar localmente, uma instância do banco de dados também local Postgres ou qualquer outro banco relacional que o laravel tenha um driver compatível (configurar o driver no arquivo de configurações .env).
 
-## Contributing
+  
 
-Thank you for considering contributing to the Laravel framework! The contribution guide can be found in the [Laravel documentation](https://laravel.com/docs/contributions).
+Para uma instância em rede, tem um Database as a Service (DBaaS) PostgreSQL gratuito no Heroku.
 
-## Security Vulnerabilities
+[Heroku postgres](https://devcenter.heroku.com/articles/heroku-postgresql)
 
-If you discover a security vulnerability within Laravel, please send an e-mail to Taylor Otwell via [taylor@laravel.com](mailto:taylor@laravel.com). All security vulnerabilities will be promptly addressed.
+  
 
-## License
+As configurações do banco de dados devem ser criadas em um arquivo chamado `.env`. O projeto tem um exemplo das requeridas configurações
 
-The Laravel framework is open-source software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+```sh
+
+$ cp .env.example .env
+
+```
+
+Editar o arquivo `.env` com as suas configurações
+
+  
+
+## Executando Localmente
+
+  
+
+  
+
+Deve ter instalado o [Composer](https://getcomposer.org/) e o [PHP >= 7.1.3](https://www.php.net/manual/en/install.php)
+
+```sh
+$ git clone https://github.com/engenhariadesoftwareuepg/projetophp.git
+$ cd projetophp
+$ composer install
+$ cp .env.example .env
+$ nano .env # ou outro editor para configurar as variáveis ambiente
+$ php artisan serve
+```
+A aplicação deverá estar executando no endereço [localhost:8000](http://localhost:5000/).
+
+## Deploying no Heroku (método 1)
+
+Deve ter instalado o [Heroku CLI](https://cli.heroku.com/)
+
+```
+$ heroku create
+$ git push heroku master
+$ heroku open
+```
+
+### Documentação do Heroku
+
+Para maiores informações segue os artigos da documentação sobre PHP no Heroku:
+
+-  [Iniciando com PHP no Heroku](https://devcenter.heroku.com/articles/getting-started-with-php)
+
+-  [Suporte Heroku PHP ](https://devcenter.heroku.com/articles/php-support)
+
+
+-  [Deploy PHP](https://devcenter.heroku.com/articles/deploying-php)
+
+ 
+  
+
+  
+
+## Realizando o deploy através do Travis-CI (método 2 - recomendado)
+
+  
+
+  
+
+Este processo é configurado pelo arquivo chamado `.travis.yml`
+
+  
+
+- Fazer um fork deste repositório
+
+  
+
+- Utilizar a sua conta no Heroku
+
+  
+
+- Fazer o login [https://id.heroku.com/login](https://id.heroku.com/login)
+
+  
+
+- Criar um novo APP com o nome que desejar (o nome deve ser único no Heroku).
+
+  
+
+- Editar o arquivo `.travis.yml` e adicionar o nome que você escolheu para a sua app:
+
+  
+
+`app: \<nome da sua app\>`
+
+  
+
+- Acessar o seu App no heroku, e na aba "Settings" e em "Config vars" configurar todas as variáveis ambientes disponíveis no .env.example para o seu ambiente de produção (com exceção da PORT, pois o heroku já possui uma variável ambiente da porta onde o servidor será instanciado)
+
+  
+
+- Acessar o seu perfil e verificar a sua API KEY (ela será utilizada posteriormente no Travis-CI)
+- Criar uma conta no [Travis-CI](https://travis-ci.org/). Utilizar a mesma conta do github "Sign in with GitHub"
+
+- Buscar o repositório do seu projeto e habilitar a integração
+
+- Clicar em "settings" do projeto e em "Environment Variables"
+- Adicionar uma variável ambiente com o name: "HEROKU_API_KEY" (sem aspas)
+- O value copiar do seu perfil no heroku.
+- Editar o arquivo .env.travis com as configurações par ao Travis-CI executar os testes
+
+Neste momento o Travis-CI já deverá estar monitorando a sua branch master.
+
+Cada vez que um push é realizado será iniciado um processo no travis, onde instanciará um ambiente transitório (temporário) para instalar a aplicação, realizar os testes automatizados e se tudo estiver corretor, automaticamente realiza o deploy na plataforma do Heroku.
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,146 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html>
 
-        <title>Laravel</title>
+<head>
+<title>Projeto de Engenharia de Software - Exemplo</title>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+<script src="{{ asset('js/app.js') }}" type="text/js"></script>
+<!--<link rel="stylesheet" type="text/css" href="/stylesheets/main.css" />-->
+<link href="{{ asset('css/main.css') }}" rel="stylesheet" type="text/css" />
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+</head>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+<body>
 
-            .full-height {
-                height: 100vh;
-            }
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Inicio<span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://devcenter.heroku.com/articles/how-heroku-works">Heroku</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://devcenter.heroku.com/articles/getting-started-with-php">Tutorial Heroku PHP</a>
+        
+      </li>
+      
+    </ul>
+  </div>
+</nav>
 
-            .position-ref {
-                position: relative;
-            }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
 
-            .content {
-                text-align: center;
-            }
+  <div class="jumbotron text-center">
+    <div class="container">
+      <a href="/" class="lang-logo">
+        <img src="{{ asset('images/lang-logo.png') }}">
+      </a>
+      <h1>Projeto de Engenharia de software</h1>
+      <p>Este é um pequeno exemplo de um CRUD simples em node de uma lista de contatos</p>
+      <a type="button" class="btn btn-lg btn-default" href="/contatos"><span class="glyphicon glyphicon-flash"></span>
+        Contatos (exemplo)</a>
+      <a type="button" class="btn btn-lg btn-primary"
+        href="https://github.com/engenhariadesoftwareuepg/projetophp"><span
+          class="glyphicon glyphicon-download"></span> Código fonte no GitHub</a>
+    </div>
+  </div>
+  <div class="container">
+    <div class="alert alert-info text-center" role="alert">
+      Para utilizar este exemplo acesse o repositório do código <a
+        href="https://github.com/engenhariadesoftwareuepg/projetophp" class="alert-link">Projeto com PHP</a>.
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-md-6">
+        <h3><span class="glyphicon glyphicon-info-sign"></span> Como este exemplo foi construido</h3>
+        <ul>
+          <li>Para o guia completo deste projeto acesse o arquivo README.md do repositório do <a
+              href="https://github.com/engenhariadesoftwareuepg/projetophp">Projeto</a>.</li>
 
-            .title {
-                font-size: 84px;
-            }
+          <li>Este projeto utiliza o framework web <a href="https://laravel.com/" target="_blank">Laravel</a></li>
+          <li>Está configurado com o banco de dados relacional PostgresSQL, utilizado o  <a href="https://laravel.com/docs/5.8/eloquent"
+              target="_blank">Eloquent ORM</a> incluído no Laravel para modelagem dos dados e mapeamento</li>
+          <li>Para a definição das views e templates é utilizado a engine <a href="https://laravel.com/docs/5.8/blade"
+              target="_blank">Blade</a></li>
+          <li>Também está implmentado alguns testes de feature com o PHPUnit. <a href="https://laravel.com/docs/5.8/testing" target="_blank">Testando no laravel</a></li>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        </ul>
+      </div>
+      <div class="col-md-6">
+        <h3><span class="glyphicon glyphicon-link"></span> Arquitetura da aplicação</h3>
+        <ul>
+          <li>Esta aplicação utiliza uma arquitetura MVC (Model, View e Control)</li>
+          <ul>
+            <li>
+              O <b>Model</b> é responsável pela estrutura de dados da aplicação, e neste caso utiliza o Eloquent Model para modelagem dos dados.
+              Os arquivos de Models fica no diretório <code>/app/</code></li>
+            <li>O <b>Controller</b> é responsável pela regra de negócio, assim como instanciar os models e gerenciar os
+              dados para renderizar as views.
+              O controller fica no diretório <code>/app/Http/controllers/</code></li>
+            <li>As <b>Views</b> tem a responsabilidade de renderizar e mostrar as informações oriundas do model e é
+              controlada pelo controller.
+              O diretório de localização das views é <code>/resources/views/</code>
+            </li>
+          </ul>
+          <li>
+            Todas as rotas, ou seja as urls que serão utilizadas para acessar cada função no sistema são definidas no
+            arquivo <code>/routes/web.php</code>
+          </li>
+          <li>
+            Os testes são executados todos arquivos que tem o sufixo Test.php que estão localizados nos diretórios  <code>/Feature/</code> e <code>/Unit/</code>
+          </li>
+        </ul>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+      </div>
+      <div class="col-md-6">
+        <h3><span class="glyphicon glyphicon-link"></span> Dicas para deploy</h3>
+        <ul>
+          <li>Utilizar o GitHub para estudantes, no qual você pode obter diversos benefícios de softwares, ferramentas e
+            plataformas com versões gratuitas, bonus, descontos, etc. Além de permitir utilizar repositórios privados
+            gratuitos. Mais informações <a href="https://education.github.com/students" target="_blank">GitHub
+              Education</a></li>
+          <li>Para automatização dos testes e integração contínua utilizar a plataforma <a href="https://travis-ci.org/"
+              target="_blank">Travis CI</a>. Para criar uma conta no serviço da plataforma deve ser utilizado a mesma
+            conta do GitHub (o travis-ci e o github são integrados entre si)</li>
+          <li>Para realizar o deploy da aplicação poderá utilizar a plataforma <a
+              href="https://www.heroku.com/home">Heroku</a>. Possui um plano básico gratuito (observar as limitações do
+            plano gratuito) que atende o desenvolvimento de projetos básicos (como é o caso da disciplina).
+            Além de também possuir um plano de estudante <a href="https://www.heroku.com/github-students" target="_blank">Heroku for GitHub Students</a>, onde a sua conta de estudante é validade pela conta do github também de estudante. Documentação, tutoriais da plataforma Heroku:
+          </li>
+          <ul>
+              <li><a href="https://devcenter.heroku.com/">Heroku Dev Center</a></li>
+              <li><a href="https://devcenter.heroku.com/articles/getting-started-with-nodejs">Iniciando com Node no
+                  Heroku</a></li>
+              <li><a href="https://devcenter.heroku.com/articles/deploying-nodejs">Fazendo deploy de aplicações node no Heroku</a></li>
+          </ul>
+          <li>
+            Para banco de dados, a mesma plataforma Heroku possui um SaaS do SGBD Postgres <a href="https://devcenter.heroku.com/articles/heroku-postgresql" target="_blank">Heroku Postgres
+            </a>
+          </li>
+          <li>
+            Para banco de dados NoSQL, um SaaS também com um plano básico gratuito (ver limitações do plano) é o <a href="https://mlab.com/" target="_blank">mLab</a> com banco de dados MongoDB
+          </li>
+          
+        </ul>
+      </div>
+    </div> <!-- row -->
+    <div class="alert alert-info text-center" role="alert">
+      Este é apenas um exemplo simples de estrutura de um projeto para a disciplina de Engenharia de software. Contém os
+      processos básicos para a criação de um CRUD (Create, Replace, Update and Delete) de registro de itens.
+      Também possui passos básicos de banco de dados, validação de entrada de dados, e testes. Seu uso não é
+      obrigatório, ficando os alunos livres para utilizarem as suas estruturas, bem como linguagem (claro atendendo as
+      necessidades do projeto e disciplina)
+    </div>
+  </div>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+</body>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
 </html>
